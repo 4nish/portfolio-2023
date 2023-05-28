c36c4e693cfb4a25fa77a81039bc264ed7caa6a3
4nish <96277294+4nish@users.noreply.github.com>
2023-05-28 07:39:33 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I am <span className='text-[#7AA874]'>Anishh</span>
+            Hi, I am <span className='text-[#7AA874]'>Anish</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am full stack web developer <br className='sm:block hidden' />
